2f61590469735faacd96478cdb67377f81090c78
Amaning Raphael Kwame Appiah <68133399+EhMaNa@users.noreply.github.com>
2022-12-01 13:07:22 -0800

    functionality for game

--- app/src/main/java/com/ehmana/kotlinmathgame/GameActivity.kt
+++ app/src/main/java/com/ehmana/kotlinmathgame/GameActivity.kt
@@ -11,6 +11,8 @@ class GameActivity : AppCompatActivity() {
 
     lateinit var binding : ActivityGameBinding
     var correctAnswer = 0
+    var userScore = 0
+    var userLife = 5
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,14 +20,32 @@ class GameActivity : AppCompatActivity() {
 
         binding = ActivityGameBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        getQuestion()
 
         binding.buttonOk.setOnClickListener {
             if (binding.answer.text.toString().isNotEmpty()){
+                if (binding.answer.text.toString().toInt() == correctAnswer) {
+                    userScore += 10
+                    binding.questionText.setText(R.string.success)
+                    binding.score.text = userScore.toString()
+                    binding.buttonOk.isClickable = false
+                } else {
+                    userLife--
+                    binding.questionText.setText(R.string.fail)
+                    binding.life.text = userLife.toString()
+                    binding.buttonOk.isClickable = false
+                }
 
             } else {
                 Toast.makeText(applicationContext,
                     "Please write an answer or click the next button", Toast.LENGTH_LONG).show()
             }
+        }
+
+        binding.buttonNxt.setOnClickListener {
+            getQuestion()
+            binding.answer.setText("")
+            binding.buttonOk.isClickable = true
         }
 
 
@@ -35,8 +55,9 @@ class GameActivity : AppCompatActivity() {
     }
 
     fun getQuestion() {
-        val number1 = Random.nextInt(10, 500)
-        val number2 = Random.nextInt(10, 500)
+        var number1 = Random.nextInt(10, 500)
+        var number2 = Random.nextInt(10, 500)
         binding.questionText.text = "$number1 + $number2"
+        correctAnswer = number1 + number2
     }
 }

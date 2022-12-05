e50669430538f6414f299ae264c8e1a33494f70f
Amaning Raphael Kwame Appiah <68133399+EhMaNa@users.noreply.github.com>
2022-12-05 10:50:18 -0800

    building result screen functionality

--- app/src/main/java/com/ehmana/kotlinmathgame/ResultActivity.kt
+++ app/src/main/java/com/ehmana/kotlinmathgame/ResultActivity.kt
@@ -1,11 +1,32 @@
 package com.ehmana.kotlinmathgame
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ehmana.kotlinmathgame.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityResultBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonPlayAgain.setOnClickListener {
+            val intent = Intent(this@ResultActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        binding.buttonExit.setOnClickListener{
+            val intent = Intent(Intent.ACTION_MAIN)
+            intent.addCategory(Intent.CATEGORY_HOME)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+        }
     }
 }

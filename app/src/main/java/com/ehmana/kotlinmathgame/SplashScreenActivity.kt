d51b7c14c8c0842d27b137cd5bac061189225df4
Amaning Raphael Kwame Appiah <68133399+EhMaNa@users.noreply.github.com>
2022-12-16 02:15:45 -0800

    add splash screen functionality

--- app/src/main/java/com/ehmana/kotlinmathgame/SplashScreenActivity.kt
+++ app/src/main/java/com/ehmana/kotlinmathgame/SplashScreenActivity.kt
@@ -2,10 +2,21 @@ package com.ehmana.kotlinmathgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import com.ehmana.kotlinmathgame.databinding.ActivitySplashScreenBinding
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivitySplashScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val animation = AnimationUtils.loadAnimation(applicationContext, R.anim.splah_anim)
+        
     }
 }

ac84473dbefcd9df60007fd10a595ff148ea951f
Amaning Raphael Kwame Appiah <68133399+EhMaNa@users.noreply.github.com>
2022-12-18 21:04:40 -1200

    update: splash screen functionality

--- app/src/main/java/com/ehmana/kotlinmathgame/SplashScreenActivity.kt
+++ app/src/main/java/com/ehmana/kotlinmathgame/SplashScreenActivity.kt
@@ -1,7 +1,10 @@
 package com.ehmana.kotlinmathgame
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.animation.AnimationUtils
 import com.ehmana.kotlinmathgame.databinding.ActivitySplashScreenBinding
 
@@ -17,6 +20,15 @@ class SplashScreenActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val animation = AnimationUtils.loadAnimation(applicationContext, R.anim.splah_anim)
-        
+        binding.appName.startAnimation(animation)
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed(object : Runnable {
+            override fun run() {
+                val intent = Intent(this@SplashScreenActivity, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }, 6000)
     }
 }
